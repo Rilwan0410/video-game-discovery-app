83c9691c8f7667053648f6137e81784b27d86844
Rilwan0410 <rilwan410@hotmail.com>
2025-01-10 10:25:51 -0500

    adding responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import { HStack } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <h2 className="text-red-500">Testing</h2>
-      <HStack>
-        <Button >Click Me!</Button>
-      </HStack>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // wider than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+        <GridItem hideBelow='lg' area="aside" bg="gold">
+          Aside
+        </GridItem>
+
+      <GridItem area="main" bg="red">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
