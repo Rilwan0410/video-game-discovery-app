4a880713d81dd7fd7aac1d1aa58ef315cfd26045
Rilwan0410 <rilwan410@hotmail.com>
2025-01-13 11:26:33 -0500

    created custom hook to fetch games from API

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,27 +1,12 @@
-import { useState, useEffect } from "react";
 import { Text } from "@chakra-ui/react";
-import apiClient from "../services/api-client";
+import useGames from "../hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const {
-          data: { results: gameData },
-        } = await apiClient.get("/games");
-        setGames(gameData);
-      } catch (err) {
-        setError(err.message);
-      }
-    })();
-  }, [games]);
+  const { games, error } = useGames();
 
   return (
     <>
-      {error && <Text>{error}</Text>} 
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
---
+++ src/hooks/useGames.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+function useGames() {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          data: { results: gameData },
+        } = await apiClient.get("/games");
+        setGames(gameData);
+      } catch (err) {
+        setError(err.message);
+      }
+    })();
+  }, [games]);
+  return { games, error };
+}
+
+export default useGames;
